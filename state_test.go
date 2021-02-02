16d65814eabf0afbe6e2a8bd0db382e3e585d761
Yan-Jie Lin <linyj.cs08g@nctu.edu.tw>
2021-02-02 16:46:37 +0800

    feat: add unit test

---
+++ state_test.go
@@ -0,0 +1,31 @@
+package fsm_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/free5gc/fsm"
+)
+
+const (
+	Opened fsm.StateType = "Opened"
+	Closed fsm.StateType = "Closed"
+)
+
+const (
+	Open  fsm.EventType = "Open"
+	Close fsm.EventType = "Close"
+)
+
+func TestState(t *testing.T) {
+	s := fsm.NewState(Closed)
+
+	assert.Equal(t, Closed, s.Current(), "Current() failed")
+	assert.True(t, s.Is(Closed), "Is() failed")
+
+	s.Set(Opened)
+
+	assert.Equal(t, Opened, s.Current(), "Current() failed")
+	assert.True(t, s.Is(Opened), "Is() failed")
+}
